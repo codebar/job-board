deed48af841578fc7f353fb42c2d33216b72310d
sara <smhumphries@hotmail.co.uk>
2021-12-08 09:46:31 +0000

    adds empty header and footer files

---
+++ code-bar-job-board-app/src/components/Firebase/firebase-creds.js
@@ -0,0 +1,9 @@
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBTvma9FYsobDlmfSdq1qcVAJVHteFW_VQ",
+    authDomain: "codebarjobboard.firebaseapp.com",
+    projectId: "codebarjobboard",
+    storageBucket: "codebarjobboard.appspot.com",
+    messagingSenderId: "672397721541",
+    appId: "1:672397721541:web:cc05c1f5aa9383e705d5ac"
+  };
