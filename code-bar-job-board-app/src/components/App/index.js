36f967c1bdc1b5bc3bb3bdd3b9b3701e8a42d14b
Sara Humphries <smhumphries@hotmail.co.uk>
2021-12-09 11:54:23 +0000

    Revert "Feature/sign up and sign in with firebase"

--- code-bar-job-board-app/src/components/App/index.js
+++ code-bar-job-board-app/src/components/App/index.js
@@ -1,33 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { collection, getDocs } from 'firebase/firestore'
-import { db, auth } from '../Firebase/firebase-config.js'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, sendSignInLinkToEmail } from 'firebase/auth';
+import { db } from '../Firebase/firebase-config.js'
 
 
 import LandingPage from '../Landing';
 import Header from '../Header';
 import Footer from '../Footer';
 import JobPage from '../JobPage/index.js';
-import SignIn from '../SignIn/index.js';
-import SignUp from '../SignUp/index.js';
-import SignOutButton from '../SignOut/index.js';
-import { NavigationBarMain, NavigationBarJobBoadNotLoggedIn, NavigationBarJobBoardLoggedIn, NavigationBarJobBoardNonLoggedIn } from '../Navigation/index.js'; 
 
 import * as ROUTES from '../../constants/routes';
 
 const App = () => {
 
     const [jobs, setJobs] = useState([]);
-    const [currentUser, setCurrentUser] = useState({});
 
     const jobsCollectionRef = collection(db, "jobs");
-
-    const actionCodeSettings = {
-      url: 'http://localhost:3000/',
-      handleCodeInApp: true,
-    };
-    
 
     useEffect (() => {
 
@@ -41,73 +29,19 @@ const App = () => {
     
       }, []);
 
-      onAuthStateChanged(auth, (currentUser) => {
-        setCurrentUser(currentUser);
-      });
-
-      const register = async (registerEmail, registerPassword) => {
-        try {
-          const user = await createUserWithEmailAndPassword(
-            auth, 
-            registerEmail, 
-            registerPassword);
-            console.log(user);
-        } catch (error) {
-          console.log(error.message);
-        };
-      };
-
-      const logIn = async (signInEmail, signInPassword) => {
-        try {
-          const user = await signInWithEmailAndPassword(
-            auth, 
-            signInEmail, 
-            signInPassword)
-          console.log(user);
-        } catch (error) {
-          console.log(error.message);
-        };
-    
-      };
-
-     
-
-      const sendLink = async (signInEmail) => {
-        await sendSignInLinkToEmail(auth, signInEmail, actionCodeSettings);
-        window.localStorage.setItem('emailForSignIn', signInEmail);
-      };
-
-      const logOut = async () => {
-        await signOut(auth);
-      };
-
 
     return (
         
             <Router>
-              <header>
-                <a href="https://slack.codebar.io/"><img alt='codebar logo' src="https://codebar.io/assets/logo-7345316d16a39b0a5cda277d2cf4cbf3aed1031b9288c0696b8273771ee1fb20.png"></img></a>
-                <NavigationBarMain />
-              
-                {currentUser?
-                  <div>
-                    <NavigationBarJobBoardLoggedIn />
-                    <p>You are signed in as {currentUser.email}</p>
-                    <SignOutButton logOut={logOut} />
-                  </div>
-                : <NavigationBarJobBoardNonLoggedIn /> }
-
-              </header>
-                
-                
+                <Header/>
             
             <hr />
             <Routes>
                   <Route exact path={ROUTES.LANDING} element={ <LandingPage jobs={jobs}/> } />
                   <Route exact path={ROUTES.JOB} element={ <JobPage></JobPage>}></Route>
-                  <Route path={ROUTES.SIGN_UP} element={ <SignUp register={register}/> } />
-                  <Route path={ROUTES.SIGN_IN} element={ <SignIn logIn={logIn} sendLink={sendLink}/> } />
-                  {/* <Route path={ROUTES.PASSWORD_FORGET} element={ <PasswordForgetPage/> } />
+                  {/* <Route path={ROUTES.SIGN_UP} element={ <SignUpPage register={register}/> } />
+                  <Route path={ROUTES.SIGN_IN} element={ <SignInPage sendLink={sendLink}/> } />
+                  <Route path={ROUTES.PASSWORD_FORGET} element={ <PasswordForgetPage/> } />
                   <Route path={ROUTES.HOME} element={<HomePage/>} />
                   <Route path={ROUTES.ACCOUNT} element={ <AccountPage/> } />
                   <Route path={ROUTES.ADMIN} element={ <AdminPage/> } /> */}
