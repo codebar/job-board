a1a2a603e79ca4d39c59b3b854b47427d176150a
Kimberley Cook <kimberleycook91@gmail.com>
2021-12-14 15:11:28 +0000

    form validation on submit job page

--- code-bar-job-board-app/src/components/BeforePostChecklist/index.js
+++ code-bar-job-board-app/src/components/BeforePostChecklist/index.js
@@ -14,48 +14,50 @@ const BeforePostCheckList = () => {
 
 
     return (
-        <div className="before-post-checklist-box">
-            <p className='bold'>Before posting make sure that:</p>
+        <div className="before-post-checklist-box card">
+            <div class="card-header">
+                <p className='bold m-0'>Before posting make sure that:</p>
+            </div>
+            <div class="card-body">
+                <Form.Group className="mb-3">
+                    <Form.Check type='checkbox' label="The position is suitable for people looking for internships, apprenticeships or junior roles that will enable them to build up their career"
+                    defaultChecked = {checkOne} onChange={() => {setCheckOne(!checkOne)}}></Form.Check>
+                </Form.Group>
 
-            <Form.Group className="mb-3">
-                <Form.Check type='checkbox' label="The position is suitable for people looking for internships, apprenticeships or junior roles that will enable them to build up their career" 
-                defaultChecked = {checkOne} onChange={() => {setCheckOne(!checkOne)}}></Form.Check>
-            </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Check type='checkbox' label="There is no degree requirement"
+                    defaultChecked = {checkTwo} onChange={() => {setCheckTwo(!checkTwo)}}></Form.Check>
+                </Form.Group>
 
-            <Form.Group className="mb-3">
-                <Form.Check type='checkbox' label="There is no degree requirement" 
-                defaultChecked = {checkTwo} onChange={() => {setCheckTwo(!checkTwo)}}></Form.Check>
-            </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Check type='checkbox' label="There is no previous experience requirement"
+                    defaultChecked = {checkThree} onChange={() => {setCheckThree(!checkThree)}}></Form.Check>
+                </Form.Group>
 
-            <Form.Group className="mb-3">
-                <Form.Check type='checkbox' label="There is no previous experience requirement"
-                defaultChecked = {checkThree} onChange={() => {setCheckThree(!checkThree)}}></Form.Check>
-            </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Check type='checkbox' label="The job description details the work that will have to be undertaken"
+                    defaultChecked = {checkFour} onChange={() => {setCheckFour(!checkFour)}}></Form.Check>
+                </Form.Group>
 
-            <Form.Group className="mb-3">
-                <Form.Check type='checkbox' label="The job description details the work that will have to be undertaken"
-                defaultChecked = {checkFour} onChange={() => {setCheckFour(!checkFour)}}></Form.Check>
-            </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Check type='checkbox' label="The job description is short and to the point"
+                    defaultChecked = {checkFive} onChange={() => {setCheckFive(!checkFive)}}></Form.Check>
+                </Form.Group>
 
-            <Form.Group className="mb-3">
-                <Form.Check type='checkbox' label="The job description is short and to the point"
-                defaultChecked = {checkFive} onChange={() => {setCheckFive(!checkFive)}}></Form.Check>
-            </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Check type='checkbox' label="The job pays an appropriate salary"
+                    defaultChecked = {checkSix} onChange={() => {setCheckSix(!checkSix)}}></Form.Check>
+                </Form.Group>
 
-            <Form.Group className="mb-3">
-                <Form.Check type='checkbox' label="The job pays an appropriate salary"
-                defaultChecked = {checkSix} onChange={() => {setCheckSix(!checkSix)}}></Form.Check>
-            </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Check type='checkbox' label="You have made the required payment of £50"
+                    defaultChecked = {checkSeven} onChange={() => {setCheckSeven(!checkSeven)}}></Form.Check>
+                </Form.Group>
 
-            <Form.Group className="mb-3">
-                <Form.Check type='checkbox' label="You have made the required payment of £50"
-                defaultChecked = {checkSeven} onChange={() => {setCheckSeven(!checkSeven)}}></Form.Check>
-            </Form.Group>
+                <Button variant='info' className='button'>Pay here</Button>
+                <p>For an additional £200 you can have your job listing featured in our newsletter and made available to an audience of over 10000 students and coaches. To find out more details and when our next newsletter is due send us an email at <a href='hello@codebar.io'>hello@codebar.io</a>.</p>
+            </div>
 
-            <Button variant='info' className='button'>Pay here</Button>
-            <p>For an additional £200 you can have your job listing featured in our newsletter and made available to an audience of over 10000 students and coaches. To find out more details and when our next newsletter is due send us an email at <a href='hello@codebar.io'>hello@codebar.io</a>.</p>
-            
-            
         </div>
     );
 };
