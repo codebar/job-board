357366f4ba936020c7a3c9f975f22ca89fffd36f
Kimberley Cook <kimberleycook91@gmail.com>
2021-12-15 10:59:42 +0000

    style no jobs component

--- code-bar-job-board-app/src/components/MyJobs/index.js
+++ code-bar-job-board-app/src/components/MyJobs/index.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import * as ROUTES from '../../constants/routes.js'
 import { Link } from 'react-router-dom';
 import SignIn from '../SignIn';
-import { Button } from 'react-bootstrap';
+import { Button, Toast } from 'react-bootstrap';
 
 
 const MyJobsPage = ({currentUser, jobs}) => {
@@ -81,28 +81,44 @@ const UserJobsPage = ({currentUserJobs}) => {
 
 const NoJobsPage = () => {
     return (
-        <main>
-            <section className="job-posting-info">
-                <h2>Reach out to our community of 9833 junior developers</h2>
-                <p>Our aim is to be the one stop shop for junior developer roles. All jobs featured on our job board must fall under the following three categories; paid internships, apprenticeships or junior developer roles. Additionally, jobs must not require previous experience or a degree and must be paid positions. Each job that is submitted will be approved by the codebar team before appearing on the job board.</p>
-                <div className="job-post-costs-section">
-                    <p>For a flat fee of £50 your post will be:</p>
-                    <ul>
-                        <li>shown on the job board for 30 days</li>
-                        <li>promoted to our Slack community of 5500 members</li>
-                        <li>promoted to our 8600 Twitter followers</li>
-                    </ul>
+        <div className="col-10">
+            <div className="row mb-4">
+                <div className="col">
+                    <h2>Reach out to our community of 9833 junior developers</h2>
+                    <p>Our aim is to be the one stop shop for junior developer roles. All jobs featured on our job board must fall under the following three categories; paid internships, apprenticeships or junior developer roles. Additionally, jobs must not require previous experience or a degree and must be paid positions. Each job that is submitted will be approved by the codebar team before appearing on the job board.</p>
+                    <div className="job-post-costs-section">
+                        <p>For a flat fee of £50 your post will be:</p>
+                        <ul>
+                            <li>Shown on the job board for 30 days</li>
+                            <li>Promoted to our Slack community of 5500 members</li>
+                            <li>Promoted to our 8600 Twitter followers</li>
+                        </ul>
+                    </div>
+                    <Link to={{
+                        pathname: ROUTES.SUBMIT_JOB
+                    }}
+                    ><Button className='button bold' variant="primary">Post a job</Button></Link>
                 </div>
-                <Link to={{
-                    pathname: ROUTES.SUBMIT_JOB
-                }}
-                ><button>Post a job</button></Link>
-            </section>
-            <section className="quote-section">
-                <p>Within 2 days of posting an ad on the codebar job board, we had dozens of high quality applications and we made an offer to a candidate within 2 weeks. There are a lot of talented interns and junior developers; posting on the codebar job board is a great way to reach them.</p>
-                <p>Bruno Girin, CTO, Imby</p>
-            </section>
-        </main>
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-4 mb-4">
+                    <Toast>
+                        <Toast.Body>
+                            <p>Within 2 days of posting an ad on the codebar job board, we had dozens of high quality applications and we made an offer to a candidate within 2 weeks. There are a lot of talented interns and junior developers; posting on the codebar job board is a great way to reach them.</p>
+                            <p>Bruno Girin, CTO, Imby</p>
+                        </Toast.Body>
+                    </Toast>
+                </div>
+                <div className="col-12 col-md-4 mb-4">
+                    <Toast>
+                        <Toast.Body>
+                            <p>The codebar job board has been amazing. It’s been our best performing job board, and the quality of the applications was awesome. 100% the best £50 we have ever spent.</p>
+                            <p>Thomas Ankcorn, NearSt</p>
+                        </Toast.Body>
+                    </Toast>
+                </div>
+            </div>
+        </div>
     );
 };
 
