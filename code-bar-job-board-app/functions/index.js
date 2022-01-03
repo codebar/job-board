7f8b86f56bd7b1a0bb4d8bc48ef1bba91662eb51
sara <smhumphries@hotmail.co.uk>
2022-01-03 11:52:54 +0000

    adds email function

--- code-bar-job-board-app/functions/index.js
+++ code-bar-job-board-app/functions/index.js
@@ -1,4 +1,35 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const sgMail = require("@sendgrid/mail");
+const app = express();
+const cors = require("cors");
+
+// const whiteList = ['https://localhost:3000', 'localhost"3000']
+
+const corsOptions = {origin: true};
+
+app.get("/", cors(corsOptions), function(request, response, next) {
+  sgMail.setApiKey(functions.config().sendgrid.key);
+  const from = "smhumphries@hotmail.co.uk";
+  const {to, subject, text} = request.query;
+  const msg = {
+    to,
+    from,
+    subject,
+    text,
+    html: `<strong>${text}</strong>`,
+  };
+  sgMail
+      .send(msg)
+      .then(() => {
+        response.status(200).send("Email sent");
+      })
+      .catch((error) => {
+        response.status(500).send(error);
+      });
+});
+
+exports.email = functions.https.onRequest(app);
 
 const admin = require("firebase-admin");
 admin.initializeApp();
