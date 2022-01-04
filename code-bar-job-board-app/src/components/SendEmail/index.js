2f95713ae399b996a13ce0ab6c033888c5ae9173
sara <smhumphries@hotmail.co.uk>
2022-01-04 14:18:50 +0000

    creates html from js for email

--- code-bar-job-board-app/src/components/SendEmail/index.js
+++ code-bar-job-board-app/src/components/SendEmail/index.js
@@ -1,5 +1,39 @@
 import { Button } from "react-bootstrap";
-import approveEmail from './approve_email.js';
+
+const job = {
+    closing_date: '20/12/2022',
+    company_address: '12, Road',
+    company_location: "London",
+    company_name: 'codebar',
+    company_postcode: 'NG3 5EY',
+    company_url: 'https://www.google.com',
+    contact_email: 'google@google.com',
+    contact_name: 'Sara Humphries',
+    created_date: new Date(),
+    creator_id: '1234',
+    job_description: 'description on the job',
+    job_post_link: 'https://www.yahoo.com',
+    job_title: "Software developer",
+    marketing_opt_in: false,
+    published_date: '04/01/2022',
+    remote: false,
+    salary: '£43,000',
+    approved_status: false
+};
+
+const approveEmail = 
+    
+        `<html style="margin: 0; padding: 0;">
+
+            <head>
+                <title>One | Email template!</title>
+            </head>
+            
+            <body style="margin: 0; padding: 0;">
+                <p>Text again third ${job.job_title}</p>
+            </body>
+            
+            </html>`
 
 const SendEmail = ({createEmail}) => {
 
