d779a6c5e3d96eea5737bdf41e85cd497b7857f7
sara <smhumphries@hotmail.co.uk>
2022-01-04 12:42:50 +0000

    creates temp send page

---
+++ code-bar-job-board-app/src/components/SendEmail/index.js
@@ -0,0 +1,11 @@
+import { Button } from "react-bootstrap";
+
+const SendEmail = ({createEmail}) => {
+    return (
+        <div className="container">
+            <Button className="button">Send</Button>
+        </div>
+    );
+};
+
+export default SendEmail;
