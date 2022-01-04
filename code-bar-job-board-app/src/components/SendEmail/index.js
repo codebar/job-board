3c04a3195b105b41943816213b003a3044ed1d3a
sara <smhumphries@hotmail.co.uk>
2022-01-04 13:45:16 +0000

    creates file for html email

--- code-bar-job-board-app/src/components/SendEmail/index.js
+++ code-bar-job-board-app/src/components/SendEmail/index.js
@@ -1,9 +1,20 @@
 import { Button } from "react-bootstrap";
+import approveEmail from './approve_email.js';
 
 const SendEmail = ({createEmail}) => {
+
+    const sendEmail = () => {
+        createEmail('smhumphries@hotmail.co.uk', 
+        {
+            subject: 'Approved!',
+            html: `${approveEmail}`,
+        });
+    };
+
+
     return (
         <div className="container">
-            <Button className="button">Send</Button>
+            <Button onClick={() => {sendEmail()}} className="button">Send</Button>
         </div>
     );
 };
