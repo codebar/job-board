bc294482a653c75e8563449e8607dea4104cc879
Sara Humphries <sara.humphries@and.digital>
2022-07-19 17:27:45 +0100

    gets one job from db

--- src/components/JobPage/index.js
+++ src/components/JobPage/index.js
@@ -1,39 +1,26 @@
 import { useEffect, useState } from "react";
 import FullJob from "../FullJob";
-import { collection, getDocs } from "firebase/firestore";
+import { getDoc, doc } from "firebase/firestore";
 import { db } from "../Firebase/firebase-config.js";
 
 const JobPage = ({ currentUser, isAdmin, approveJob, unPublishJob }) => {
-  const [jobs, setJobs] = useState([]);
-  const [focusJob, setFocusJob] = useState({});
+  const [job, setJob] = useState({});
 
-  const jobsCollectionRef = collection(db, "jobs");
+  const jobRef = doc(db, "jobs", window.location.href.split("/").pop());
 
   useEffect(() => {
-    const getJobs = async () => {
-      const jobsData = await getDocs(jobsCollectionRef);
-      setJobs(jobsData.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    const getJob = async () => {
+      const jobData = await getDoc(jobRef);
+      setJob(jobData.data(), jobData.id);
     };
 
-    const findJobFromURL = () => {
-      const url = window.location.href;
-      const jobId = url.split("/").pop();
-      for (let job of jobs) {
-        if (job.id === jobId) {
-          setFocusJob(job);
-        }
-      }
-    };
-
-    getJobs();
-    findJobFromURL();
-  }, [jobsCollectionRef, jobs]);
-
+    getJob();
+  }, [jobRef]);
 
   return (
     <div>
       <FullJob
-        job={focusJob}
+        job={job}
         currentUser={currentUser}
         isAdmin={isAdmin}
         unPublishJob={unPublishJob}
