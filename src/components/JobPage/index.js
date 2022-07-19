b32d11b84701be76f8d8d9fb783e50849df80628
Sara Humphries <sara.humphries@and.digital>
2022-07-19 17:05:02 +0100

    re-sends jobs from db to job page

--- src/components/JobPage/index.js
+++ src/components/JobPage/index.js
@@ -1,19 +1,46 @@
-import { useLocation } from 'react-router'
-import FullJob from '../FullJob';
+import { useEffect, useState } from "react";
+import FullJob from "../FullJob";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../Firebase/firebase-config.js";
+
+const JobPage = ({ currentUser, isAdmin, approveJob, unPublishJob }) => {
+  const [jobs, setJobs] = useState([]);
+  const [focusJob, setFocusJob] = useState({});
+
+  const jobsCollectionRef = collection(db, "jobs");
+
+  useEffect(() => {
+    const getJobs = async () => {
+      const jobsData = await getDocs(jobsCollectionRef);
+      setJobs(jobsData.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    const findJobFromURL = () => {
+      const url = window.location.href;
+      const jobId = url.split("/").pop();
+      for (let job of jobs) {
+        if (job.id === jobId) {
+          setFocusJob(job);
+        }
+      }
+    };
+
+    getJobs();
+    findJobFromURL();
+  }, [jobsCollectionRef, jobs]);
 
 
-const JobPage = ({currentUser, isAdmin, approveJob, unPublishJob}) => {
-
-    const data = useLocation();
-    const job = data.state.job;
-
-    return (
-        <FullJob job={job} currentUser={currentUser} isAdmin={isAdmin} unPublishJob={unPublishJob} approveJob={approveJob}></FullJob>
-    );
+  return (
+    <div>
+      <FullJob
+        job={focusJob}
+        currentUser={currentUser}
+        isAdmin={isAdmin}
+        unPublishJob={unPublishJob}
+        approveJob={approveJob}
+      ></FullJob>
+    </div>
+  );
 };
-
-
-
-
 
 export default JobPage;
